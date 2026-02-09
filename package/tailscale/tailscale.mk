0fc5e9bab4ce6dee9151bde33acf84cfec119b34
Abylay Ospan <aospan@sbnb.io>
2026-02-09 15:55:29 -0500

    package/tailscale: fix merged /usr compatibility
    When BR2_ROOTFS_MERGED_USR is enabled (required by systemd), /usr/sbin
    is a symlink to bin (i.e., /usr/bin). The TAILSCALE_INSTALL_SYMLINK
    hook creates /usr/sbin/tailscaled -> ../bin/tailscaled, which actually
    creates the file in /usr/bin/ (following the symlink), overwriting the
    real binary with a circular symlink.
    
    Fix by only creating the symlink when merged /usr is not enabled,
    following the same pattern used by iputils and other packages.
    
    Signed-off-by: Abylay Ospan <aospan@sbnb.io>

--- package/tailscale/tailscale.mk
+++ package/tailscale/tailscale.mk
@@ -22,10 +22,15 @@ define TAILSCALE_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/usr/lib/systemd/system/tailscaled.service
 endef
 
+# Only create /usr/sbin symlink when merged /usr is not enabled.
+# With merged /usr, /usr/sbin is a symlink to bin, so this would
+# overwrite the actual binary with a circular symlink.
+ifeq ($(BR2_ROOTFS_MERGED_USR),)
 define TAILSCALE_INSTALL_SYMLINK
 	ln -f -s ../bin/tailscaled $(TARGET_DIR)/usr/sbin/tailscaled
 endef
 TAILSCALE_POST_INSTALL_TARGET_HOOKS += TAILSCALE_INSTALL_SYMLINK
+endif
 
 define TAILSCALE_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_IPV6)
